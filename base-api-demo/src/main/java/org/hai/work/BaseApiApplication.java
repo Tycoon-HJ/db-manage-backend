5e133c43e68dae6b1c03280a4d934fbf58931a8d
yinhaijun <2523284817@qq.com>
2023-05-03 16:57:04 +0800

    feat:新增nacos的配置中心

--- base-api-demo/src/main/java/org/hai/work/BaseApiApplication.java
+++ base-api-demo/src/main/java/org/hai/work/BaseApiApplication.java
@@ -1,5 +1,7 @@
 package org.hai.work;
 
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableDubbo
 @SpringBootApplication
 @EnableAsync
+@EnableNacosDiscovery
+@EnableNacosConfig
 public class BaseApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApiApplication.class, args);
--- base-api-demo/src/main/java/org/hai/work/application/resource/AsyncDemo.java
+++ base-api-demo/src/main/java/org/hai/work/application/resource/AsyncDemo.java
@@ -1,5 +1,6 @@
 package org.hai.work.application.resource;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AsyncDemo {
 
+    // autoRefreshed 需要和配置文件里的auto-refresh配合使用
+    @NacosValue(value = "${username}", autoRefreshed = true)
+    private String username;
+
     /**
      * 每次会开启一个新的线程：SimpleAsyncTaskExecutor
      * 不建议使用，因为无法对线程池进行控制
@@ -21,6 +26,6 @@ public class AsyncDemo {
     @GetMapping("/xx")
     @Async
     public void newThread() {
-        log.info("11111" + Thread.currentThread().getName());
+        log.info("11111" + Thread.currentThread().getName() + this.username);
     }
 }
